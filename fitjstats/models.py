e06f3d6cfe5eccbe8650439b5573b9407928ac80
Kevin Lau <laudevelopment@gmail.com>
2018-11-06 20:58:42 -0800

    Prepare for db migrate

--- fitjstats/models.py
+++ fitjstats/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Workout(mondels.Model):
-    title = models.CharField(default="")
-    description = models.CharField()
-    tags = models.CharField(default="")
+class Workout(models.Model):
+    title = models.CharField(default="", max_length=200)
+    description = models.TextField()
+    tags = models.TextField()
 
 class Post(models.Model):
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
@@ -11,17 +11,23 @@ class Post(models.Model):
     url = models.URLField()
     num_comments = models.IntegerField(default=0)
 
+class Commenter(models.Model):
+    first_name = models.CharField(default="", max_length=200) 
+    last_name = models.CharField(default="", max_length=200) 
+    picture_url = models.URLField()
+    created = models.DateField()
+
 class Comment(models.Model):
-    RX = "rx"
-    SCALE = "scale"
+    RX = "RX"
+    SCALE = "SC"
     SCALE_CHOICES = (
         (None, "n/a"),
         (RX, "rx"),
         (SCALE, "scale")
     )
-    MALE = "m"
-    FEMALE = "f"
-    OTHER = "o"
+    MALE = "MAL"
+    FEMALE = "FEM"
+    OTHER = "OTH"
     GENDER_CHOICES = (
         (None, "n/a"),
         (MALE, "male"),
@@ -29,18 +35,13 @@ class Comment(models.Model):
         (OTHER, "other")
     )
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    commenter = models.ForeignKey(Commenter, on_delete=models.SET_NULL)
-    comment_text = models.CharField()
-    scale = models.CharField(choices=SCALE_CHOICES)
-    gender = models.CharField(choices=GENDER_CHOICES)
-    raw_score = models.CharField(default="")
+    commenter = models.ForeignKey(Commenter, null=True, on_delete=models.SET_NULL)
+    comment_text = models.TextField()
+    scale = models.CharField(choices=SCALE_CHOICES, max_length=2)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=3)
+    raw_score = models.CharField(default="", max_length=200)
     score = models.IntegerField(null=True)
     height = models.IntegerField(null=True)
     weight = models.IntegerField(null=True)
     age = models.IntegerField(null=True)
 
-class Commenter(models.Model):
-    first_name = models.CharField(default="") 
-    last_name = models.CharField(default="") 
-    picture_url = models.URLField()
-    created = models.DateField()
--- fitjstats/settings.py
+++ fitjstats/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crawl.apps.CrawlConfig',
+    'fitjstats.apps.FitjstatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
