e70c832e5bc3237755146a1701615acaa22915f8
Kevin Lau <laudevelopment@gmail.com>
2018-11-13 19:10:10 -0800

    Add naive filter and consolidate onclick actions to one js function

--- fitjstats/static/crawl.js
+++ fitjstats/static/crawl.js
@@ -11,6 +11,9 @@ function showToday() {
     document.getElementsByName("DD")[0].value = day;
 }
 
+var btnToday = document.getElementById("getToday");
+btnToday.addEventListener("click", showToday());
+
 function display(id, dates) {
     document.getElementById(id).innerHTML = "";
     for (var i = 0; i < dates.length; i++) {
@@ -41,11 +44,36 @@ function showCalendar() {
     display("calendar", dates);
 }
 
-function flex(e) {
-    if (e.classList.contains('collapsed')) {
-        e.classList.replace('collapsed', 'expanded');
+function showOnly(e, cls) {
+    if (e.classList.contains('inactive')) {
+        e.classList.replace('inactive', 'active');
     } else {
-        e.classList.replace('expanded', 'collapsed');
+        e.classList.replace('active', 'inactive');
+    }
+    var cards = document.getElementsByClassName("card");
+    for (var i=1; i<cards.length; i++) {
+        if (!cards[i].classList.contains(cls)) {
+            if (cards[i].style.display != "none") {
+                cards[i].style.display = "none";
+            } else {
+                cards[i].style.display = "flex";
+            }
+        }
     }
 }
+
+function addFlex(selector) {
+    var items = document.getElementsByClassName(selector);
+    for (var i=0; i<items.length; i++) {
+        items[i].addEventListener("click", function() {
+            if (this.classList.contains('collapsed')) {
+                this.classList.replace('collapsed', 'expanded');
+            } else {
+                this.classList.replace('expanded', 'collapsed');
+            }
+        });
+    }
+}
+
+addFlex("card");
 
